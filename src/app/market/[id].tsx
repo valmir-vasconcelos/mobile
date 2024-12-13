d3fb7a77c6cd3a3addeeac06456c2fee049b93e1
Valmir Vasconcelos <valmir.bsi@gmail.com>
2024-12-12 23:02:20 -0300

    app finalizado

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/button";
 import { Loading } from "@/components/loading";
+import { Coupon } from "@/components/market/coupon";
 import { Cover } from "@/components/market/cover";
 import { Details, PropsDetails } from "@/components/market/details";
 import { api } from "@/services/api";
+import { CameraView, useCameraPermissions } from "expo-camera";
 import { Redirect, router, useLocalSearchParams } from "expo-router";
-import { useEffect, useState } from "react";
-import { Alert, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Alert, Modal, ScrollView, StatusBar, View } from "react-native";
 
 type DataProps = PropsDetails & {
     cover: string
@@ -14,8 +17,14 @@ export default function Market() {
 
     const [data, setData] = useState<DataProps>();
     const [isLoading, setIsLoading] = useState(true);
-
+    const [coupon, setCoupon] = useState<string | null>(null);
+    const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false);
+    const [couponIsFetching, setCouponIsFetching] = useState(false);
+    //const [permission, requestPermission] = useCameraPermissions();
+    const [_, requestPermission] = useCameraPermissions();
     const params = useLocalSearchParams<{ id: string }>();
+
+    const qrLock = useRef(false)
 
     async function fetchMarket() {
         try {
@@ -30,9 +39,48 @@ export default function Market() {
         }
     }
 
+    async function handleOpenCamera() {
+        try {
+            const { granted } = await requestPermission();
+            if (!granted) {
+                return Alert.alert("Câmera", "Você precisa habilitar o uso da câmera")
+            }
+            setIsVisibleCameraModal(true)
+            qrLock.current = false
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Câmera", "Não foi possível utilizar a câmera")
+        }
+    }
+
+    async function getCoupon(id: string) {
+        try {
+            setCouponIsFetching(true)
+            const { data } = await api.patch(`/coupons/${id}`)
+            setCoupon(data.coupon)
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Erro", "Não foi possível utilizar o cupom")
+        } finally {
+            setCouponIsFetching(false)
+        }
+    }
+
+    function handleUseCoupon(id: string) {
+        setIsVisibleCameraModal(false)
+        Alert.alert("Cupom",
+            "Não será possível reutilizar um cupom resgatado. Deseja realmente resgatar o cupom?",
+            [
+                { style: "cancel", text: "Não" },
+                { text: "Sim", onPress: () => getCoupon(id) }
+            ]
+        )
+    }
+
+
     useEffect(() => {
         fetchMarket();
-    }, [params.id])
+    }, [params.id, coupon])
 
     if (isLoading) {
         return <Loading />
@@ -44,8 +92,36 @@ export default function Market() {
 
     return (
         <View style={{ flex: 1, }}>
-            <Cover uri={data.cover} />
-            <Details data={data} />
+            <StatusBar barStyle="light-content" hidden={isVisibleCameraModal} />
+            <ScrollView showsVerticalScrollIndicator={false} >
+                <Cover uri={data.cover} />
+                <Details data={data} />
+                {coupon && <Coupon code={coupon} />}
+            </ScrollView>
+
+            <View style={{ padding: 32 }} >
+                <Button onPress={handleOpenCamera}>
+                    <Button.Title>Ler QR Code</Button.Title>
+                </Button>
+            </View>
+
+            <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+                <CameraView
+                    style={{ flex: 1, justifyContent: "center" }}
+                    facing="back"
+                    onBarcodeScanned={({ data }) => {
+                        if (data && !qrLock.current) {
+                            qrLock.current = true
+                            setTimeout(() => handleUseCoupon(data), 500);
+                        }
+                    }}
+                />
+                <View style={{ position: "absolute", bottom: 32, left: 32, right: 32 }}>
+                    <Button onPress={() => setIsVisibleCameraModal(false)} isLoading={couponIsFetching}>
+                        <Button.Title>Voltar</Button.Title>
+                    </Button>
+                </View>
+            </Modal>
         </View>
     )
 }
--- src/components/market/details/index.tsx
+++ src/components/market/details/index.tsx
@@ -1,5 +1,7 @@
 import { Text, View } from "react-native"
 import { s } from "./style"
+import { Info } from "../info"
+import { IconMapPin, IconPhone, IconTicket } from "@tabler/icons-react-native"
 
 export type PropsDetails = {
     name: string,
@@ -25,6 +27,19 @@ export function Details({ data }: Props) {
 
             <View style={s.group}>
                 <Text style={s.title}>Informações</Text>
+                <Info icon={IconTicket} description={`${data.coupons} cupons disponíveis`} />
+                <Info icon={IconMapPin} description={data.address} />
+                <Info icon={IconPhone} description={data.phone} />
+            </View>
+            <View style={s.group}>
+                <Text style={s.title}>Regulamento</Text>
+                {
+                    data.rules.map((item) => (
+                        <Text key={item.id} style={s.rule}>
+                            {`\u2022 ${item.description}`}
+                        </Text>
+                    ))
+                }
             </View>
         </View>
     )
